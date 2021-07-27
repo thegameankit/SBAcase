27f19c01b0b9aea6c3dcaeadffac6db80676d8d7
thegameankit <ankitgarg041998@gmail.com>
2021-07-27 16:46:06 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Controller {
+	@GetMapping("/hello")
+	public String simple()
+	{
+		return "Hello world";
+	}
+}
